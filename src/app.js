bccf33a0352703f8572881e40f7d32a3c1023008
LoetRomijn <loetromijn@gmail.com>
2016-02-16 20:09:08 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var jade = require('jade');
 
 var connectionString = "postgres://" + process.env.POSTGRES_USER + ":@localhost/bulletinboard";
 
